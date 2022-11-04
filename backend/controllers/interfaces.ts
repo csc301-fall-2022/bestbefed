f44cc822ee3354eabc6d90a78585a73c70bc8619
KevinL <kkevin.li@mail.utoronto.ca>
2022-11-04 18:44:10 -0400

    added entity classes and changed existing entities, also modified some previous files because i changed the entitiy names a bit

--- backend/controllers/interfaces.ts
+++ backend/controllers/interfaces.ts
@@ -1,25 +1,25 @@
 // Collection of interfaces used by the controllers
 export interface PaymentInfo {
-    creditCard: string;
-    expiryDate: string;
-    cvv: string;
+  creditCard: string;
+  expiryDate: string;
+  cvv: string;
 }
 
 export interface UserRequest {
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    paymentInfo: PaymentInfo;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  paymentInfo: PaymentInfo;
 }
 
 export interface UserErrors {
-    numErrors: number;
-    username: string;
-    password: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    paymentInfo: string[],
+  numErrors: number;
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  paymentInfo: string[];
 }
