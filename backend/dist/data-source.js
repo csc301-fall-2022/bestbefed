9f83c2a955f067add170432006cd5086c7996a87
Matthew Parvaneh <mattparvaneh@outlook.com>
2022-11-05 22:45:08 -0400

    Changing image in docker-compose.yml from postgres to postgis

--- backend/dist/data-source.js
+++ backend/dist/data-source.js
@@ -11,10 +11,12 @@ const OrderedItem_1 = require("./entity/OrderedItem");
 const Store_1 = require("./entity/Store");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "bestbefed-data.czbbb7d5g36e.us-east-2.rds.amazonaws.com",
+    // host: "bestbefed-data.czbbb7d5g36e.us-east-2.rds.amazonaws.com",
+    host: "localhost",
     port: 5432,
     username: "postgres",
-    password: "12345678",
+    // password: "12345678",
+    password: "postgres",
     database: "postgres",
     entities: [User_1.User, Order_1.Order, Inventory_1.Inventory, CartItem_1.CartItem, OrderedItem_1.OrderedItem, Store_1.Store],
     synchronize: true,
