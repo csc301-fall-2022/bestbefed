6a62ab3d980e7792d1ee0afac1a9b94abe208a9e
sack6oy <tobey.lbp@gmail.com>
2022-11-06 14:00:09 -0500

    Updating API tests

---
+++ backend/__tests__/routes.t.ts
@@ -0,0 +1,37 @@
+// // import express from 'express';
+// import router from '../routes/user';
+// import {describe, expect, test} from '@jest/globals';
+// const request = require('supertest');
+// const express = require('express');
+// var bodyParser = require('body-parser');
+
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use('/', router);
+
+describe('Not implemented yet.', function () {
+  test('Not implemented yet.', () => {
+      }); 
+});
+
+// describe('Test User Routes', function () {
+  
+//   test('POST to /user/', async () => {
+//   // Store the response from the user creation
+//   const res = await request(app).post('/')
+//   .send({ 
+//       username: "heyhellotesttest",
+//       password: "LovelyLif3$",
+//       firstName: "Matty",
+//       lastName: "P",
+//       email: "bigmattyp@aol.com",
+//       paymentInfo: {
+//         "creditCard": "4485011320017596",
+//         "expiryDate": "12/24",
+//         "cvv": "923"
+//     }
+//   });
+//   expect(res.statusCode).toBe(201);
+//   });
+// }); 
