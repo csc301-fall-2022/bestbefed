ab7c1ba9f8c4c11f15399ed30d8a5eb7831ae4cf
1izardo <daniel@dlazaro.ca>
2022-11-06 12:39:42 -0500

    Update Axios URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    process.env.PRODUCTION == "true"
+    process.env.NODE_ENV == "production"
       ? "https://app.bestbefed.ca"
       : "http://localhost:8000",
 });
