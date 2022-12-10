31f9bc4705f1ef609f8372b1af3b256400b11d75
sack6oy <tobey.lbp@gmail.com>
2022-12-10 13:07:50 -0500

    Setup Order backend implementation

--- backend/dist/controllers/user.js
+++ backend/dist/controllers/user.js
@@ -73,21 +73,23 @@ const cleanUser = (newUser) => __awaiter(void 0, void 0, void 0, function* () {
         errors.numErrors += 1;
         errors["lastName"] = "Please enter your last name.";
     }
-    // Check that every piece of the payment info is valid (if provided)
-    if (newUser.paymentInfo.creditCard ||
-        newUser.paymentInfo.cvv ||
-        newUser.paymentInfo.expiryDate) {
-        if (!validator_1.default.isCreditCard(newUser.paymentInfo.creditCard)) {
-            errors.numErrors += 1;
-            errors["paymentInfo"].push("Please enter a valid credit card number!");
-        }
-        //   if (!validator.isDate(newUser.paymentInfo.expiryDate)) {
-        //     errors["paymentInfo"].push("Please enter a valid card expiry date!");
-        //   }
-        if (newUser.paymentInfo.cvv.length != 3 ||
-            !validator_1.default.isNumeric(newUser.paymentInfo.cvv)) {
-            errors.numErrors += 1;
-            errors["paymentInfo"].push("Please enter a valid CVV code for your credit card.");
+    if (newUser.paymentInfo) {
+        // Check that every piece of the payment info is valid (if provided)
+        if (newUser.paymentInfo.creditCard ||
+            newUser.paymentInfo.cvv ||
+            newUser.paymentInfo.expiryDate) {
+            if (!validator_1.default.isCreditCard(newUser.paymentInfo.creditCard)) {
+                errors.numErrors += 1;
+                errors["paymentInfo"].push("Please enter a valid credit card number!");
+            }
+            //   if (!validator.isDate(newUser.paymentInfo.expiryDate)) {
+            //     errors["paymentInfo"].push("Please enter a valid card expiry date!");
+            //   }
+            if (newUser.paymentInfo.cvv.length != 3 ||
+                !validator_1.default.isNumeric(newUser.paymentInfo.cvv)) {
+                errors.numErrors += 1;
+                errors["paymentInfo"].push("Please enter a valid CVV code for your credit card.");
+            }
         }
     }
     // Needs to return either the cleaned user or errors dictionary
@@ -106,18 +108,14 @@ const cleanUser = (newUser) => __awaiter(void 0, void 0, void 0, function* () {
  * @return { UserRequest }
  */
 const constructUserRequest = (requestBody) => __awaiter(void 0, void 0, void 0, function* () {
-    const userData = {
-        username: requestBody.body.username.trim(),
-        password: requestBody.body.password.trim(),
-        firstName: requestBody.body.firstName.trim(),
-        lastName: requestBody.body.lastName.trim(),
-        email: requestBody.body.email.trim(),
+    var _a, _b, _c;
+    const userData = Object.assign({ username: requestBody.body.username.trim(), password: requestBody.body.password.trim(), firstName: requestBody.body.firstName.trim(), lastName: requestBody.body.lastName.trim(), email: requestBody.body.email.trim() }, (requestBody.body.paymentInfo && {
         paymentInfo: {
-            creditCard: requestBody.body.paymentInfo.creditCard.trim(),
-            expiryDate: requestBody.body.paymentInfo.expiryDate,
-            cvv: requestBody.body.paymentInfo.cvv.trim(),
+            creditCard: ((_a = requestBody.body.paymentInfo.creditCard) === null || _a === void 0 ? void 0 : _a.trim()) || "",
+            expiryDate: ((_b = requestBody.body.paymentInfo.expiryDate) === null || _b === void 0 ? void 0 : _b.trim()) || "",
+            cvv: ((_c = requestBody.body.paymentInfo.cvv) === null || _c === void 0 ? void 0 : _c.trim()) || "",
         },
-    };
+    }));
     return userData;
 });
 /**
@@ -150,13 +148,24 @@ const createUser = (req, res) => __awaiter(void 0, void 0, void 0, function* () 
         newUser.password = hashedPass;
         newUser.email_verified = false;
         newUser.create_date = new Date();
-        newUser.payment_info = user.paymentInfo; // this is bad practice - but we know it'll implement the interface if we get here
+        if (user.paymentInfo) {
+            newUser.payment_info = user.paymentInfo; // this is bad practice - but we know it'll implement the interface if we get here
+        }
+        else {
+            // If payment info not provided, just make it blank
+            newUser.payment_info = {
+                creditCard: "",
+                expiryDate: "",
+                cvv: "",
+            };
+        }
         yield userRepository.save(newUser);
         // Send back 201 upon successful creation
         res.status(201).json("New user created.");
     }
     catch (err) {
-        res.status(500).send(err);
+        console.log(err);
+        res.status(500).json(err);
     }
 });
 exports.createUser = createUser;
