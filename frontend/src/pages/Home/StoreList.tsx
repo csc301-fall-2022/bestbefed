56386050d0f1eb856605606b5b1fb33acb59397a
1izardo <daniel@dlazaro.ca>
2022-12-10 15:33:25 -0500

    Implement link to backend for map

--- frontend/src/pages/Home/StoreList.tsx
+++ frontend/src/pages/Home/StoreList.tsx
@@ -1,73 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import StoreListItem from "./StoreListItem";
 import { Container } from "react-bootstrap";
-import axios from "../../api/axios";
-
-const POST_STORE_URL = "/store/stores";
+import { IMapContext, MapContext } from "./MapContextProvider";
 export interface Store {
   id: string;
   name: string;
-  category: string;
+  category: string | null;
   distance: number | null;
   description: string;
 }
 
-function StoreList({
-  query,
-  curLocation,
-}: {
-  query: string;
-  curLocation: mapboxgl.LngLat | null;
-}) {
-  const [stores, setStores] = useState([]);
-  const [noneFound, setNoneFound] = useState(false);
-
-  const getStores = async () => {
-    axios
-      .get(POST_STORE_URL, {
-        headers: { "Content-Type": "application/json" },
-        withCredentials: true,
-        params: {
-          storeName: query,
-          ...(curLocation && { location: curLocation.toArray() }),
-        },
-      })
-      .then((response) => {
-        setStores(response.data);
-        setNoneFound(false);
-      })
-      .catch((err) => {
-        console.error("Unable to fetch stores");
-        setNoneFound(true);
-      });
-  };
+function StoreList({ query }: { query: string }) {
+  const { stores, getStores } = useContext(
+    MapContext as React.Context<IMapContext>
+  );
 
   useEffect(() => {
     getStores();
-  }, [query, curLocation]);
+  }, [query]);
 
-  if (noneFound || stores.length === 0) {
-    return (
-      <Container className="d-flex flex-grow-1 store-list px-0 align-items-center justify-content-center">
-        <div className="fs-5 fst-italic text-secondary">No stores found</div>
-      </Container>
-    );
-  }
   return (
-    <Container className="flex-grow-1 store-list px-0 pt-3">
-      {stores.map(({ id, address, storeName, distance, description }, i) => {
-        return (
-          <StoreListItem
-            id={id}
-            name={storeName}
-            category="Grocery"
-            distance={curLocation ? distance : null}
-            description={address}
-            key={id}
-          />
-        );
-      })}
-    </Container>
+    <>
+      {stores.features.length === 0 ? (
+        <Container className="d-flex flex-grow-1 store-list px-0 align-items-center justify-content-center">
+          <div className="fs-5 fst-italic text-secondary">No stores found</div>
+        </Container>
+      ) : (
+        <Container className="flex-grow-1 store-list px-0 mt-3 pe-2">
+          {stores.features.map((store) => {
+            return <StoreListItem store={store} key={store.properties!.id} />;
+          })}
+        </Container>
+      )}
+    </>
   );
 }
 
