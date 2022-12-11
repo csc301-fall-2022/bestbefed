f0e5cff637b98eda4718ffa45b5c364f14fb2c4e
1izardo <daniel@dlazaro.ca>
2022-12-10 22:07:43 -0500

    Add total rounding to Review component

--- frontend/src/pages/Cart/Checkout/Review.tsx
+++ frontend/src/pages/Cart/Checkout/Review.tsx
@@ -1,71 +1,58 @@
-import "./review.css"
+import "./review.css";
 
 interface CartItem {
-    cart_item_id: number;
-    name: string;
-    store: string;
-    quantity: number;
-    price: number;
-    inventory_item: number;
-    imageUrl: string;
-  }
+  cart_item_id: number;
+  name: string;
+  store: string;
+  quantity: number;
+  price: number;
+  inventory_item: number;
+  imageUrl: string;
+}
 
 interface ReviewProps {
-    items: CartItem[],
-    total: number
+  items: CartItem[];
+  total: number;
 }
 
-
-
-function Reveiw(props: ReviewProps){
-    console.log(props.items)
-    return (  
+function Review(props: ReviewProps) {
+  return (
     <>
- 
-        <div className="rowtop">
-            <div className="col">
+      <div className="rowtop">
+        <div className="col">
+          <h3 className="row">ITEM</h3>
 
-                <h3 className="row">
-                            ITEM
-                </h3>
-
-                {props.items.map((item) =>
-                    <span key={item.cart_item_id} className="row">
-                        {item.name}
-                    </span>)
-                }               
-            </div>
-
-            <div className="col">
-
-                <h3 className="row">
-                            PRICE
-                </h3>
-
-                {props.items.map((item) =>
-                    <span key={item.cart_item_id} className="row">
-                        {`$ ` + item.price}
-                    </span>)
-                }               
-            </div>
-
-            <div className="col">
-
-                <h3 className="row">
-                            QUANTITY
-                </h3>
-                {props.items.map((item) =>
-                    <span key={item.cart_item_id} className="row">
-                        {item.quantity}
-                    </span>)
-                }               
-            </div>
+          {props.items.map((item) => (
+            <span key={item.cart_item_id} className="row">
+              {item.name}
+            </span>
+          ))}
         </div>
 
-        <h3 style={{textAlign: "center", marginTop: "1em", color: "black"}}>
-            Total with taxes: {` $` + (props.total * 1.13)}
-        </h3>
-    </> 
-    )
+        <div className="col">
+          <h3 className="row">PRICE</h3>
+
+          {props.items.map((item) => (
+            <span key={item.cart_item_id} className="row">
+              {`$ ` + item.price}
+            </span>
+          ))}
+        </div>
+
+        <div className="col">
+          <h3 className="row">QUANTITY</h3>
+          {props.items.map((item) => (
+            <span key={item.cart_item_id} className="row">
+              {item.quantity}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <h3 style={{ textAlign: "center", marginTop: "1em", color: "black" }}>
+        Total with taxes: {` $${(props.total * 1.13).toFixed(2)}`}
+      </h3>
+    </>
+  );
 }
-export default Reveiw
+export default Review;
